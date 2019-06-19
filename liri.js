ccd3e1371367210f9826af9ead40200cc9008076
BAANG <chcelestino@gmail.com>
2019-06-18 23:46:58 -0700

    Add functioning spotify api request

--- liri.js
+++ liri.js
@@ -2,10 +2,12 @@
 
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api")
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios")
 var fs = require("fs")
 var moment = require("moment")
+var chalk = require("chalk")
 
 // Global Variables
 var parseUserInput = "";
@@ -26,22 +28,22 @@ console.log(userSearch)
 // Switch-case that runs functions based on user input
 switch (userInput) {
     case "concert-this":
-        console.log("Searching for concerts...")
+        console.log("\nSearching for concerts...\n")
         findConcert();
         break;
 
     case "spotify-this-song":
-        console.log("Searching for song...")
+        console.log("\nSearching for song...\n")
         findSong();
         break;
 
     case "movie-this" :
-        console.log("Searching for movie...")    
+        console.log("\nSearching for movie...\n")    
         findMovie();
         break;
 
     case "do-what-it-says" :
-        console.log("Do what it says...")
+        console.log("\nDo what it says...\n")
         doWhatItSays();
         break;
 } 
@@ -51,14 +53,26 @@ function findConcert() {
     var queryUrl = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp";
     
     axios.get(queryUrl).then(function(response){
-        console.log("Found a concert for " + response.data[0].lineup[0] + "...")
-        console.log(response.data[0].venue.name)
-        console.log(response.data[0].venue.city + ", " + response.data[0].venue.region)
+        console.log("Found a concert for " + chalk.inverse(response.data[0].lineup[0] + "...\n"))
+        console.log(chalk.underline(response.data[0].venue.name))
+        console.log("@ " + response.data[0].venue.city + ", " + response.data[0].venue.region)
         console.log(moment(response.data[0].datetime).format("dddd, MMMM Do YYYY, h:mm a"))
     })
     .catch(function(error) {
         console.log(error);
     });
+}
+
+// Spotify API
+function findSong() {
+    var queryUrl = "https://api.spotify.com/v1/search?q=track:" + userSearch + "&type=track&limit=3"
+    spotify.request(queryUrl).then(function(response) {
+        console.log(response.tracks.items[0].name)
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+
 }
 
 
